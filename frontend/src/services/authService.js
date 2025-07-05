6b5a2f244849c1a3b4dd2176e370b9067e8e6dde
Namita <namita3599@gmail.com>
2025-07-05 17:50:04 +0530

    Temp commit for rebase continue

--- frontend/src/services/authService.js
+++
@@ -1,7 +0,0 @@
-import axios from '../api/axios';
-
-const register = (data) => axios.post('/auth/register', data);
-const login = (data) => axios.post('/auth/login', data);
-
-const authService = { register, login };
-export default authService;
